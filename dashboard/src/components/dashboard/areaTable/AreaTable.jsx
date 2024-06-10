a0be23da49eac958af4c5f89341d9bd233d883ad
koulienathalie <koulienathalie@icloud.com>
2024-06-10 08:08:04 +0300

    table update

--- dashboard/src/components/dashboard/areaTable/AreaTable.jsx
+++ dashboard/src/components/dashboard/areaTable/AreaTable.jsx
@@ -2,69 +2,40 @@ import AreaTableAction from "./AreaTableAction";
 import "./AreaTable.scss";
 
 const TABLE_HEADS = [
-  "Products",
-  "Order ID",
-  "Date",
-  "Customer name",
-  "Status",
-  "Amount",
-  "Action",
+  "User",
+  "Traffic",
 ];
 
 const TABLE_DATA = [
   {
     id: 100,
-    name: "Iphone 13 Pro",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "delivered",
-    amount: 400,
+    user: "192.168.110.171",
+    traffic: "44.8 KB",
   },
   {
     id: 101,
-    name: "Macbook Pro",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "pending",
-    amount: 288,
+    user: "172.22.64.56",
+    traffic: "24.3 KB",
   },
   {
     id: 102,
-    name: "Apple Watch",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "canceled",
-    amount: 500,
+    user: "172.22.86.35",
+    traffic: "17.3 KB",
   },
   {
     id: 103,
-    name: "Microsoft Book",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "delivered",
-    amount: 100,
+    user: "172.22.64.154",
+    traffic: "15.1 KB",
   },
   {
     id: 104,
-    name: "Apple Pen",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "delivered",
-    amount: 60,
+    user: "172.22.64.164",
+    traffic: "10.2 KB",
   },
   {
     id: 105,
-    name: "Airpods",
-    order_id: 11232,
-    date: "Jun 29,2022",
-    customer: "Afaq Karim",
-    status: "delivered",
-    amount: 80,
+    user: "172.22.68.111",
+    traffic: "6.4 KB",
   },
 ];
 
@@ -72,7 +43,7 @@ const AreaTable = () => {
   return (
     <section className="content-area-table">
       <div className="data-table-info">
-        <h4 className="data-table-title">Latest Orders</h4>
+        <h4 className="data-table-title">Top Users</h4>
       </div>
       <div className="data-table-diagram">
         <table>
@@ -87,22 +58,9 @@ const AreaTable = () => {
             {TABLE_DATA?.map((dataItem) => {
               return (
                 <tr key={dataItem.id}>
-                  <td>{dataItem.name}</td>
-                  <td>{dataItem.order_id}</td>
-                  <td>{dataItem.date}</td>
-                  <td>{dataItem.customer}</td>
-                  <td>
-                    <div className="dt-status">
-                      <span
-                        className={`dt-status-dot dot-${dataItem.status}`}
-                      ></span>
-                      <span className="dt-status-text">{dataItem.status}</span>
-                    </div>
-                  </td>
-                  <td>${dataItem.amount.toFixed(2)}</td>
-                  <td className="dt-cell-action">
+                  <td>{dataItem.user}</td>
+                  <td>{dataItem.traffic}</td>
                     <AreaTableAction />
-                  </td>
                 </tr>
               );
             })}
